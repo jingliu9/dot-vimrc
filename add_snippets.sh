2726d0613a39b7c0f16943a5e704716fb8e49ead
jingliu9 <jingliu.cs@gmail.com>
2022-02-01 20:23:01 -0600

    small fix of add_snippets.sh

--- add_snippets.sh
+++ add_snippets.sh
@@ -26,7 +26,7 @@ test_existence colort $MD_SNIPPETS
 retval=$?
 if [ "$retval" == 0 ]
 then
-    print '\n'
+    printf '\n'
     printf '\nsnippet colort\n\t <span style="color: ${1:color};">${0:text}</span>' >> $MD_SNIPPETS
 fi
 
